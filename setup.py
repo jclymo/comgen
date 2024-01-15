0aaa0d6e7c4bd073cfcbe46a3de70b10f81c5d85
Jude <judith.clymo@gmail.com>
2024-01-15 15:43:51 +0000

    update setup file

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup
 
 setup(
     name="comgen", 
-    version="0.0.13",
-    description="explore ionic composition space",
-    packages=['comgen', 'comgen.constraintsystems', 'comgen.data'], 
-    package_dir={'':'src'},
-    package_data={'comgen.data': ['periodic_table.json', 'common_poly_ions.txt']},    
+    version="0.0.15",
+    description="explore chemical compositions",
+    packages=['comgen'],#, 'comgen.constraint_systems', 'comgen.constraint_systems.composition' 'comgen.data'], 
+    # package_dir={'':'src'},
+    # package_data={'comgen.data': ['periodic_table.json', 'common_poly_ions.txt']},    
     install_requires=['pymatgen>=2022.5.26', 'z3-solver>=4.8.17.0']
 )
