a6a868ebfcdeb65e918b9973e93e13d88802ecdd
Jude <judith.clymo@gmail.com>
2024-02-18 18:32:54 -0800

    manage fractional values

--- comgen/query/base.py
+++ comgen/query/base.py
@@ -1,9 +1,15 @@
-from z3 import sat, Solver
+from z3 import sat, Solver, Q
+from fractions import Fraction
 
 class Query:
     def __init__(self):
         self.constraints = []
         self.solutions = [] 
+
+    def frac_to_rational(self, val):
+        if isinstance(val, Fraction):
+            return Q(val.numerator, val.denominator)
+        return val
 
     def get_next(self):
         s = Solver() 
